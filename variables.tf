0ad30d0e5b0972ae1cdca0ada0d6bb764f3f4827
pinkypea <90901787+pinkypea@users.noreply.github.com>
2024-04-09 14:40:38 +0700

    add ovf_file

--- variables.tf
+++ variables.tf
@@ -20,8 +20,12 @@ variable "esxi_password" {
 
 variable "esxi_diskstore" {
 	default = "datastore1"
-  
 }
+
+variable "ovf_file" {
+  default =  "https://cloud-images.ubuntu.com/releases/jammy/release/ubuntu-22.04-server-cloudimg-amd64.ova"
+}
+
 variable "vmIP" {
   default = "192.168.1.110/24"
 }
